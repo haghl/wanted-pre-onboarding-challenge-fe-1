7cf6ba453ccbd9dff5e434816a4dcaf28e5c8ba0
Haghl <wdg980813@gmail.com>
2023-01-05 11:05:59 +0900

    수정

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -3,10 +3,7 @@ import { getToDoList } from '@/api'
 import { AppBar, Box, Button, Container, CssBaseline, List, ListItem, ListItemText, Toolbar, Typography } from '@mui/material'
 import moment from 'moment'
 import { useNavigate } from 'react-router-dom'
-<<<<<<< HEAD
-=======
 import ToDoPlus from '@/components/ToDoPlus'
->>>>>>> ff61f15 (새벽 수정)
 
 type TodoListProp = { title: string; content: string; id: string; createdAt: string; updatedAt: string }
 
