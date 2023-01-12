9dd669b3db21f5e9df1deb6d6e65fc9c7945985b
Haghl <wdg980813@gmail.com>
2023-01-12 20:21:39 +0900

    refactor(src/types) : 타입 파일 구분

---
+++ src/types/route.ts
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export interface IRoutes {
+  id: number
+  path: string
+  title: string
+  component: React.ReactNode
+}
---
+++ src/types/todo.ts
@@ -0,0 +1,4 @@
+export interface ITodoProps {
+  title: string
+  content: string
+}
